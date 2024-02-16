4c5a9f3b8ebde060f75c3dc5c4f6dee00d9b6217
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2024-02-15 22:06:35 -0500

    randomize fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,7 +35,16 @@ return false;
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+  // keep track of who goes first
+var isPlayerTurn = true;
+if (Math.random() > 0.5) {
+  isPlayerTurn = false;
+} 
+
+
+
   while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) { 
      // ask player if they'd like to fight or skip using fightOrSkip function
 
   if(fightOrSkip()){
@@ -58,6 +67,8 @@ var fight = function(enemy) {
     } else {
       window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
     }
+   // player gets attacked first
+  } else {
     // remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
     playerInfo.health = Math.max(0, playerInfo.health - damage);
@@ -73,6 +84,9 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+   // switch turn order for next round
+   isPlayerTurn = !isPlayerTurn;
+}
 };
 // function to start a new game
 var startGame = function() {
